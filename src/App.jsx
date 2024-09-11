1be006ff8186ea335acdfd3673be14c7c8d91f4a
SharizHussain <shariz.hussain384@gmail.com>
2024-09-11 21:50:02 +0530

    light mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import backBtnImg from './assets/img/undo.png'
 import imgHistory from './assets/img/chat(1).png'
 import imgCatg from './assets/img/menu.png'
 import imgArchive from './assets/img/archive.png'
+import sunImg from './assets/img/sun.png'
 import './App.css'
 
 function App() {
+  const [dark, setDark] = useState(true);
   const [menu, setMenu] = useState("Menu")
 
   useEffect(()=>{
@@ -26,56 +28,53 @@ function App() {
     }
   },[menu])
 
-  const menuFun = (e) =>{
-      setMenu(e)
+  
+  const togglebar = () =>{
+    const toggleBtn = document.querySelector('.toggleBtn')
+    toggleBtn.classList.toggle('toggleOff')
   }
-
-  useEffect(() => {
-
-    const backBtn = document.querySelector(".backBtn");
-    backBtn.addEventListener('click',()=>{
-      setMenu("Menu")
-    })
-
-  }, [])
 
   return (
     <>
                               {/* Header */}
-      <header className='header'>
+      <header className={`header ${dark ? "" : "lightMode"}`}>
         <div className='navLeft'>
           <img src={img} alt="icon" className='icon'/>
           <img src={img2} alt="title" className='title'/>
         </div>
 
         <div className='navRight'>
-          <div className='theme'></div>
+          <div className='theme'>
+            <img src={sunImg} alt="" className='sun' onClick={()=>setDark(!dark)}/>
+          </div>
           <div className='profile'></div>
         </div>
       </header>
 
                               {/* Sidebar */}
-      <section className='sidebar'>
-        <img src={backBtnImg} alt="" className='backBtn'/>
-        <div className='currentMenu'> {menu} </div>
+      <section className={`sidebar ${dark ? '' : "lightMode"}`}>
+        <img src={backBtnImg} alt="" className='backBtn' onClick={()=>setMenu("Menu")}/>
+        <div className={`currentMenu ${dark ? "" : "lightMode"}`}> {menu} </div>
 
       {
 <>    
 {menu==="Menu" ?
         <div className='menuTabs'>
-            <div onClick={() => menuFun("Chat History")}> <img src={imgHistory} alt="" className='icons'/> <span> Chat History </span> </div>
-            <div onClick={() => menuFun("Categories")}> <img src={imgCatg} alt="" className='icons'/> <span> Chat Categories </span> </div>
-            <div onClick={() => menuFun("Archives")}> <img src={imgArchive} alt="" className='icons'/> <span> Archives </span></div>
+            <div onClick={() => setMenu("Chat History")}> <img src={imgHistory} alt="" className='icons'/> <span> Chat History </span> </div>
+            <div onClick={() => setMenu("Categories")}> <img src={imgCatg} alt="" className='icons'/> <span> Chat Categories </span> </div>
+            <div onClick={() => setMenu("Archives")}> <img src={imgArchive} alt="" className='icons'/> <span> Archives </span></div>
         </div>
 :
         <Chats prop={menu}/>
 }
 </>
         }
+
+      <button className='sidebarToggle' onClick={togglebar}><div className='toggleBtn'></div></button>
       </section>
                                 
                               {/* Main */}
-      <main>
+      <main className={`${dark ? "" : "lightMode"}`}>
         {/* <input type="text" className='mainInput' placeholder='Type your query here....'/> */}
         <textarea name="" id="" className='mainInput' placeholder='Type your query here....'></textarea>
       </main>
